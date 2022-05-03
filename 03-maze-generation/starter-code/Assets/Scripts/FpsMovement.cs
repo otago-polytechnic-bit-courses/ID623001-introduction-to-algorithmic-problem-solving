8412df21b8888f2fdb223c49fc61d4e7f715fc1a
Adon <adon.moskal@gmail.com>
2022-05-03 15:28:11 +1200

    adding starter code for maze generation project

---
+++ 03-maze-generation/starter-code/Assets/Scripts/FpsMovement.cs
@@ -0,0 +1,72 @@
+﻿/*
+ * written by Joseph Hocking 2017
+ * released under MIT license
+ * text of license https://opensource.org/licenses/MIT
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+// basic WASD-style movement control
+public class FpsMovement : MonoBehaviour
+{
+    [SerializeField] private Camera headCam;
+
+    public float speed = 6.0f;
+    public float gravity = -9.8f;
+
+    public float sensitivityHor = 9.0f;
+    public float sensitivityVert = 9.0f;
+
+    public float minimumVert = -45.0f;
+    public float maximumVert = 45.0f;
+
+    private float rotationVert = 0;
+
+    private CharacterController charController;
+
+    void Start()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        MoveCharacter();
+        RotateCharacter();
+        RotateCamera();
+    }
+
+    private void MoveCharacter()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed);
+
+        movement.y = gravity;
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+
+        charController.Move(movement);
+    }
+
+    private void RotateCharacter()
+    {
+        transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityHor, 0);
+    }
+
+    private void RotateCamera()
+    {
+        rotationVert -= Input.GetAxis("Mouse Y") * sensitivityVert;
+        rotationVert = Mathf.Clamp(rotationVert, minimumVert, maximumVert);
+
+        headCam.transform.localEulerAngles = new Vector3(
+            rotationVert, headCam.transform.localEulerAngles.y, 0
+        );
+    }
+}
---
+++ 03-maze-generation/starter-code/Assets/Scripts/TriggerEventRouter.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * written by Joseph Hocking 2017
+ * released under MIT license
+ * text of license https://opensource.org/licenses/MIT
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void TriggerEventHandler(GameObject trigger, GameObject other);
+
+public class TriggerEventRouter : MonoBehaviour
+{
+    public TriggerEventHandler callback;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (callback != null)
+        {
+            callback(this.gameObject, other.gameObject);
+        }
+    }
+}
